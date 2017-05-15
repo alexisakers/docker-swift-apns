87364cb2c718a5c892298550808029a972792aa4
Alexis Aubry Radanovic <aleks@alexis-aubry-radanovic.me>
2017-05-15 10:40:28 +0200

    Fix Package name

--- .ci/Package.swift
+++ .ci/Package.swift
@@ -1,7 +1,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "dsa",
+    name: "docker-swift-apns",
     dependencies: [
         .Package(url: "https://github.com/JohnSundell/Files.git", majorVersion: 1, minor: 8)
     ]
