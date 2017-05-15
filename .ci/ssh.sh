db72c4ba037aeea860212e26afe88e692c932541
Alexis Aubry Radanovic <aleks@alexis-aubry-radanovic.me>
2017-05-15 09:48:18 +0200

    Add passphrase automatically

--- .ci/ssh.sh
+++ .ci/ssh.sh
@@ -2,6 +2,7 @@
 echo -e "Host *\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
 echo -e "ssh-rsa $SSH_KEY $SSH_EMAIL" > ~/.ssh/id_rsa.pub
 echo -e "$RSA_PRIVKEY" > ~/.ssh/id_rsa
+echo -e "$RSA_PRIVKEY_PASS" > ~/.ssh/passfile
 chmod 600 ~/.ssh/id_rsa
 eval `ssh-agent -s`
-ssh-add ~/.ssh/id_rsa
+cat ~/.ssh/passfile | ssh-add ~/.ssh/id_rsa
