6f7f1bd7c761d0a11afd948565052581d437de08
Alexis Aubry Radanovic <aleks@alexis-aubry-radanovic.me>
2017-07-29 13:18:36 +0200

    Fix CI for latest updates
    - Export Swift version of the script as a separate variable
    - Fix CoreFoundation permission error in Travis

--- .ci/install.sh
+++ .ci/install.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
+set -e
+
 echo "👉  Installing Swift"
-SWIFT_URL=https://swift.org/builds/$SWIFT_BRANCH/ubuntu1404/$SWIFT_VERSION/$SWIFT_VERSION-ubuntu14.04.tar.gz
+SWIFT_URL=https://swift.org/builds/$LOCAL_SWIFT_BRANCH/ubuntu1404/$LOCAL_SWIFT_VERSION/$LOCAL_SWIFT_VERSION-ubuntu14.04.tar.gz
 curl -fSsL $SWIFT_URL -o swift.tar.gz
 sudo tar -xzf swift.tar.gz --directory /usr/local --strip-components=2
+
+echo "👉  Fixing CoreFoundation"
+sudo find /usr/local/lib/swift/CoreFoundation -type f -exec chmod 644 {} \;
 
 echo "👉  Compiling Script"
 swiftc docker-swift-apns.swift
