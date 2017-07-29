6fa1dfc205f53d04df14a59acabc91193c028ac0
Alexis Aubry Radanovic <aleks@alexis-aubry-radanovic.me>
2017-07-29 13:26:13 +0200

    Fix CI script path

--- .ci/install.sh
+++ .ci/install.sh
@@ -3,13 +3,13 @@ set -e
 
 echo "👉  Installing Swift"
 SWIFT_URL=https://swift.org/builds/$LOCAL_SWIFT_BRANCH/ubuntu1404/$LOCAL_SWIFT_VERSION/$LOCAL_SWIFT_VERSION-ubuntu14.04.tar.gz
-curl -fSsL $SWIFT_URL -o swift.tar.gz
+curl -L $SWIFT_URL -o swift.tar.gz
 sudo tar -xzf swift.tar.gz --directory /usr/local --strip-components=2
 
 echo "👉  Fixing CoreFoundation"
 sudo find /usr/local/lib/swift/CoreFoundation -type f -exec chmod 644 {} \;
 
 echo "👉  Compiling Script"
-swiftc docker-swift-apns.swift
+swiftc docker-swift-apns.swift -o ~/docker-swift-apns
 
 echo "✅  All dependencies installed successfully"
