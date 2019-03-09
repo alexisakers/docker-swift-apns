3d47e9d9018cda019dc5de486aec6cf7204e25a8
Alexis Aubry <me@alexaubry.fr>
2019-03-09 12:48:35 -0800

    Update tests and install steps

--- .ci/install.sh
+++ .ci/install.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 set -e
 
+sudo apt-get install clang libicu-dev
+
 echo "👉  Installing Swift"
-SWIFT_URL=https://swift.org/builds/$LOCAL_SWIFT_BRANCH/ubuntu1404/$LOCAL_SWIFT_VERSION/$LOCAL_SWIFT_VERSION-ubuntu14.04.tar.gz
+SWIFT_URL=https://swift.org/builds/$LOCAL_SWIFT_BRANCH/ubuntu1404/$LOCAL_SWIFT_VERSION/$LOCAL_SWIFT_VERSION.tar.gz
 curl -L $SWIFT_URL -o swift.tar.gz
 sudo tar -xzf swift.tar.gz --directory /usr/local --strip-components=2
 
